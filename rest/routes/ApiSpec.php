d784f5169ffd203153517f3ee5b85029d3cfe213
Nadja Cizmic <nadjacizmic@hotmail.com>
2023-07-09 19:12:37 +0200

    deploy link added

--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -4,6 +4,7 @@
  * @OA\Info(title="Web project", version="0.2", @OA\Contact(email="web", name="Web"))
  * @OA\OpenApi(
  *    @OA\Server(url="http://localhost/Restaurant-reservation-system/rest", description="Development Environment" ),
+ *    @OA\Server(url="https://restaurant-reservation-system-eight.vercel.app/", description="Production Environment" )
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
  */
